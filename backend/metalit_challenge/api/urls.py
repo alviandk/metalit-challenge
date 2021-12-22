ca1f73046beb12f641797aa666677a509c6f3bed
fadelananda <fadelananda123@gmail.com>
2021-12-22 19:08:16 +0700

    add task verification api

--- backend/metalit_challenge/api/urls.py
+++ backend/metalit_challenge/api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import ChallengeView, TaskView, ChallengeTaskView
+from django.urls import path, include
+from rest_framework import routers
+from .views import ChallengeView, TaskView, ChallengeTaskView, TaskVerificationViewSet
+
+router = routers.DefaultRouter()
+router.register(r'task-verification', TaskVerificationViewSet, basename='task-verification')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('challenge', ChallengeView.as_view()),
     path('task/<int:challenge_id>', TaskView.as_view()),
     path('challengetask/<int:challenge_id>', ChallengeTaskView.as_view()),
--- backend/metalit_challenge/api/views.py
+++ backend/metalit_challenge/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import serializers, status
+from rest_framework.decorators import action
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from .models import Challenge, Task
-from .serializers import ChallengeSerializer, TaskSerializer
+from .models import Challenge, Task, TaskVerification
+from .serializers import ChallengeSerializer, TaskSerializer, TaskVerificationSerializer
 
 # Create your views here.
 
@@ -49,3 +51,31 @@ class ChallengeTaskView(APIView):
     if challenge_query.count() == 0 and task_query.count() == 0:
       return Response({"description": "challenge ID not found"}, status=status.HTTP_404_NOT_FOUND)
     return Response({"challenge": challenge_serializers.data, "tasks": task_serializers.data})
+
+"""Using ViewSet on Task Verification"""
+class TaskVerificationViewSet(ViewSet):
+  """
+  GET all task verification
+  """
+  def list(self, request):
+    queryset = TaskVerification.objects.all()
+    serializers = TaskVerificationSerializer(queryset, many=True)
+    return Response(serializers.data)
+  
+  """
+  GET unverified task
+  """
+  @action(detail=False, methods=['GET'])
+  def unverified(self, request):
+    query = TaskVerification.objects.filter(is_verified=False)
+    serializers = TaskVerificationSerializer(query, many=True)
+    return Response(serializers.data)
+  
+  """
+  GET verified task
+  """
+  @action(detail=False, methods=['GET'])
+  def verified(self, request):
+    query = TaskVerification.objects.filter(is_verified=True)
+    serializers = TaskVerificationSerializer(query, many=True)
+    return Response(serializers.data)
