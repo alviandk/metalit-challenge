c3d5ac6c18bd7432e4a6d70234b1b947ef28c5f5
fadelananda <fadelananda123@gmail.com>
2021-12-23 11:31:13 +0700

    change library
    change python-dotenv to environs

--- backend/metalit_challenge/metalit_challenge/settings.py
+++ backend/metalit_challenge/metalit_challenge/settings.py
@@ -10,24 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
+from environs import Env
 from pathlib import Path
-from dotenv import load_dotenv
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('ALLOWED_HOST')]
 
 
 # Application definition
@@ -85,11 +87,11 @@ DATABASES = {
     # }
     'default': {
         'ENGINE'    : 'django.db.backends.mysql',
-        'NAME'      : os.getenv('NAME'),
-        'USER'      : os.getenv('USER'),
-        'PASSWORD'  : os.getenv('PASSWORD'),
-        'HOST'      : os.getenv('HOST'),
-        'PORT'      : os.getenv('PORT')
+        'NAME'      : env('NAME'),
+        'USER'      : env('USER'),
+        'PASSWORD'  : env('PASSWORD'),
+        'HOST'      : env('HOST'),
+        'PORT'      : env('PORT')
     }
 }
 
