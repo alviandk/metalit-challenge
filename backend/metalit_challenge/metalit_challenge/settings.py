63b12e5c9e39fe0fbb9cb6df82a3fbf82c00be08
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2021-12-29 13:52:22 +0700

    list

--- backend/metalit_challenge/metalit_challenge/settings.py
+++ backend/metalit_challenge/metalit_challenge/settings.py
@@ -10,24 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
+from environs import Env
 from pathlib import Path
-from dotenv import load_dotenv
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('ALLOWED_HOST')]
 
 
 # Application definition
@@ -43,6 +45,11 @@ INSTALLED_APPS = [
     'api',
     'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -77,21 +84,31 @@ WSGI_APPLICATION = 'metalit_challenge.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+DB_ENGINE = env('DJANGO_DB_ENGINE', default='mysql')
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE'    : 'django.db.backends.mysql',
-        'NAME'      : os.getenv('NAME'),
-        'USER'      : os.getenv('USER'),
-        'PASSWORD'  : os.getenv('PASSWORD'),
-        'HOST'      : os.getenv('HOST'),
-        'PORT'      : os.getenv('PORT')
+if DB_ENGINE == 'sqlite':
+    DB_NAME = env('DB_NAME', default=BASE_DIR / 'db.sqlite3')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': DB_NAME,
+        }
     }
-}
+else:
+    DATABASES = {
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': BASE_DIR / 'db.sqlite3',
+        # }
+        'default': {
+            'ENGINE'    : 'django.db.backends.mysql',
+            'NAME'      : env('NAME'),
+            'USER'      : env('USER'),
+            'PASSWORD'  : env('PASSWORD'),
+            'HOST'      : env('HOST'),
+            'PORT'      : env('PORT')
+        }
+    }
 
 
 # Password validation
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import Coba from "./components/Coba";
+import Coba from "./container/ProgressChallenge";
+import Progress from "./components/Coba";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import './App.css';
+import Navig from "./components/Navbar";
+import Footer from "./components/Footer";
+import './index.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+      	<Navig />
         <Switch>
           <Route path="/" component={Coba} exact />
+          <Route path="/a" component={Progress} exact />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
